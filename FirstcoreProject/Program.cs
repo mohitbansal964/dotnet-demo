b25412f1b06ee2de39ee58d2ee023402347b1a96
Mohit Bansal <bansalmohit72@gmail.com>
2020-09-08 10:45:42 +0530

    Fixed code

--- FirstcoreProject/Program.cs
+++ FirstcoreProject/Program.cs
@@ -9,19 +9,9 @@ namespace FirstcoreProject
         {
             Console.WriteLine("Hello World!");
             System.Console.WriteLine("I am in .net project");
-
-            var ob;
-            string choice = "add";
-            switch(choice){
-                case "add" : ob = new Addition();
-                ob.display();
-                break;
-
-                case "sub" : ob = new Subtraction();
-                ob.display();
-                break;
-
-            }
+            var ob = new Addition();
+            ob.display();
+                
         }
     }
 }
